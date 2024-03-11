4e567e3995439616d48a88541117bf111f14919e
Rachael Judy <rjudy@cedarville.edu>
2024-03-11 17:22:45 -0400

    add neighbor function

--- HW2/automata.py
+++ HW2/automata.py
@@ -30,27 +30,28 @@ import random
 from utils import get_x_from_px, display_2D_automata
 
 
-def random_move(layer, position: tuple, k: int, q: int) -> tuple:
+def random_move(layer, agent: int, agent_positions: dict, k: int, q: int) -> tuple:
     """
     Base case of searching up to q empty positions and moves to first where happy or if not possible, happiest possible
     :param layer: current state of the cellular automata
-    :param position: tuple (i, j) of the agent in question
+    :param agent: agent whose position is being explored around
+    :param agent_positions: dictionary of all agents to their current position
     :param q: number of empty positions to search
     :param k: required number of like neighbors to be happy
     :return: tuple (i, j) of recommended spot to move
     """
     # collect empty positions
-    empty_positions = {position}
+    empty_positions = {agent_positions[agent]}
     for i in range(len(layer)):
         for j in range(len(layer[0])):
             if layer[i][j] == 0:
                 empty_positions.add((i, j))
 
     # look over q empty spots for one which would provide maximum happiness
-    best_spot, happiest = position, -1
+    best_spot, happiest = agent_positions[agent], -1
     for _ in range(q):
         potential_location = random.sample(list(empty_positions), 1)[0]
-        happiness = get_happiness(layer, potential_location, layer[position[0]][position[1]], k)
+        happiness = get_happiness(layer, potential_location, layer[agent_positions[agent][0]][agent_positions[agent][1]], k)
         if happiness == 1:
             return potential_location
         if happiness > happiest:
@@ -60,9 +61,26 @@ def random_move(layer, position: tuple, k: int, q: int) -> tuple:
     return best_spot
 
 
-def social_network_recommendation(layer, position: tuple, k: int, n: int, p: int) -> tuple:
-    # we're going to yield instead of return so we can
-    pass
+def social_network_recommendation(layer, agent: int, agent_positions: dict, k: int, n: int, p: int, q: int, friend_map) -> tuple:
+    # assuming suitable means perfectly happy
+    # we're going to yield instead of return so we can set up the neighbors
+    # assign every agent a unique identifier from the start and a group of n friends
+    # Use None as a sentinel value to trigger initialization
+    curri, currj = agent_positions[agent]
+    recommendations = set()
+    for friend in friend_map[agent]:
+        fi, fj = agent_positions[friend]
+        for i in range(-p // 2, p // 2 + 1):
+            for j in range(-p // 2, p // 2 + 1):
+                if (layer[(fi + i) % len(layer)][(fj + j) % len(layer)] == 0
+                        and get_happiness(layer, (fi + i, fj + j), layer[curri][currj], k) == 1):
+                    recommendations.add(((fi + i) % len(layer), (fj + j) % len(layer)))
+    if recommendations:
+        # print(random.sample(list(recommendations), 1)[0])
+        return random.sample(list(recommendations), 1)[0]
+    else:
+        return random_move(layer, agent, agent_positions, k, q)
+
 
 def rachael_move():
     pass
@@ -126,12 +144,19 @@ def simulate_automata(L: int, alpha: float, k: int, epochs: int = 20, trials: in
         time_series = [[[get_x_from_px([0, 1, 2], [1-alpha, alpha/2, alpha/2])
                          for _ in range(L)] for _ in range(L)]]
         epoch_to_scores[0].append(rate_performance(time_series[0], k))  # note initial score
-        agents = [i for i in range(L*L)]  # create list to sample from for randomly moving an agent
+
+        # create list to sample from for randomly moving an agent - agents are assigned index based on initial position
+        agents = list(filter(lambda val: time_series[0][val // L][val % L] != 0, [i for i in range(L*L)]))
+        agent_positions = {agent: (agent // L, agent % L) for agent in agents}
+
+        # TODO: Add necessary initialziation for each method
+        if relocation_policy == 1:
+            friend_map = {a: random.sample(agents, policy_parameters[0]) for a in agents}
 
         for epoch in range(epochs):
             random.shuffle(agents)
             for agent in agents:
-                i, j = agent // L, agent % L
+                i, j = agent_positions[agent]
                 # square does not contain an agent, ignore
                 if time_series[-1][i][j] == 0:
                     continue
@@ -141,9 +166,12 @@ def simulate_automata(L: int, alpha: float, k: int, epochs: int = 20, trials: in
                 if get_happiness(time_series[-1], (i,j), time_series[-1][i][j], k) != 1:
                     # move function returns the new location
                     if relocation_policy == 0:
-                        new_i, new_j = policies[0](layer=time_series[-1], position=(i,j), k=k, q=policy_parameters[0])
+                        new_i, new_j = policies[0](layer=time_series[-1], agent=agent, agent_positions=agent_positions,
+                                                   k=k, q=policy_parameters[0])
                     elif relocation_policy == 1:
-                        policies[1]()
+                        new_i, new_j = policies[1](layer=time_series[-1],  agent=agent, agent_positions=agent_positions,
+                                                   k=k, n=policy_parameters[0], p=policy_parameters[1], q=policy_parameters[2],
+                                                   friend_map=friend_map)
                     elif relocation_policy == 2:
                         policies[2]()
                     elif relocation_policy == 3:
@@ -151,10 +179,12 @@ def simulate_automata(L: int, alpha: float, k: int, epochs: int = 20, trials: in
                     elif relocation_policy == 4:
                         policies[4]()
 
-                    # move to new position
+                    # move to new position and adjust visual as well
+                    agent_positions[agent] = (new_i, new_j)
                     environment_copy = deepcopy(time_series[-1])
                     environment_copy[new_i][new_j], environment_copy[i][j] = environment_copy[i][j], 0
                     time_series.append(environment_copy)
+
             # at end of each epoch, score the performance
             epoch_to_scores[epoch + 1].append(rate_performance(time_series[-1], k))
 
@@ -165,5 +195,12 @@ def simulate_automata(L: int, alpha: float, k: int, epochs: int = 20, trials: in
 
 policies = {0: random_move, 1: social_network_recommendation, 2: rachael_move, 3: connor_move, 4: josh_move}
 
-results = simulate_automata(L=40, alpha=.9, k=3, epochs=20, trials=20, relocation_policy=0, policy_parameters=[100], display_flag=False)
+# test random
+results = simulate_automata(L=40, alpha=.9, k=5, epochs=20, trials=20, relocation_policy=0, policy_parameters=[100], display_flag=True)
+
+# test social
+results = simulate_automata(L=40, alpha=.9, k=3, epochs=20, trials=20, relocation_policy=1, policy_parameters=[10, 5, 100], display_flag=True)
+
 print(np.average(results[20]))
+
+# TODO: add plotting code for averages/standard deviations
