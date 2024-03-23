1a8732c89c56fec11e1a8252217ae8ce309a91c2
Rachael Judy <rjudy@cedarville.edu>
2024-03-23 18:36:49 -0400

    create simulation with plugins for risk and data feed

--- Code/simulation.py
+++ Code/simulation.py
@@ -1,6 +1,8 @@
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
+import powerlaw
 import random
 
 from broker import Broker
@@ -11,17 +13,54 @@ broker_network = nx.DiGraph()
 # we need to pull in data over relevant intervals - maybe
 # create list of brokers
 N = 100
-brokers = [Broker(i, 100) for i in range(N)]
+standard_influence = .2
+time_steps = 1000
+epochs = 100
 
-broker_network.add_nodes_from([node.id for node in brokers])
+# figure out what data to collect and plot from the simulation
 
-# need three simulations? different days/times frequency allowed to trade,
+for epoch in range(epochs):
+    # (self, id: int, initial_money: float, risk_minimum: float=0, risk_maximum: float=math.inf,
+    #                  purchase_rate_limit: int=0, neighbor_weight: float=0, shift_influence = False)
+    # brokers will need to set these values based on if we're doing a influence shift or a risk variety
 
-# add edges somehow setting influence with power law distribution? - need to better define influence though
-# How should the influence be adjusted?
-broker_network.add_edges_from([(2, 3, {'weight': 8})])
+    brokers = [Broker(i,  100) for i in range(N)]
 
-# change with broker_network[source][dest][weight]
+    # Generate some synthetic data following a power law distribution
+    friend_count_dist = np.floor(np.array(powerlaw.Power_Law(xmin=1, parameters=[3.0]).generate_random(N)) + 2)
 
-nx.draw(broker_network, with_labels=True, font_color='white', node_shape='s')
-plt.show()
+    """
+    # Plot the histogram of the generated samples
+    plt.hist(samples, bins=50, color='g', alpha=0.5, density=True)
+    plt.xlabel('Value')
+    plt.ylabel('Probability')
+    plt.title('Histogram of Samples from Power Law Distribution')
+    plt.show()
+    """
+
+    # create network and populate friends
+    broker_network.add_nodes_from([node.id for node in brokers])
+    for i in range(N):
+        for j in range(int(friend_count_dist[i])):
+            neighbor = np.random.normal(N/2, .34*N)
+            broker_network.add_edge(i, neighbor, weight=standard_influence/friend_count_dist[i])
+
+
+    nx.draw(broker_network, with_labels=True, font_color='white', node_shape='s')
+    plt.show()
+
+    for step in range(time_steps):
+        random.shuffle(brokers)
+        # TODO: replace with data for the time step, possible turn into map of stock to price/quantity available
+        available_stocks = {}
+
+        for broker in brokers:
+            broker.update(broker_network, available_stocks)
+
+    # examine state of brokers at end
+
+    for broker in brokers:
+        print(broker.get_status())
+
+# add plotting code from whatever we decide to plot
+
