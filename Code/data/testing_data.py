90890b365b26df3b725fab449c35a66d260c9684
Connor Klein <kleinc3@mail.uc.edu>
2024-04-05 20:22:12 -0400

    Everything but the csv

---
+++ Code/data/testing_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import ast
+
+# All the columns. Dates is a 1 to 1 ratio to timeseries
+"""
+catagories = ['Ticker',
+              'dates', 
+              'time_series'
+              'auditRisk', 
+              'boardRisk', 'compensationRisk', 'shareHolderRightsRisk', 'overallRisk', 'governanceEpochDate', 'compensationAsOfEpochDate', 'maxAge', 'previousClose', 'open', 'dayLow', 'dayHigh', 'regularMarketPreviousClose', 'regularMarketOpen', 'regularMarketDayLow', 'regularMarketDayHigh', 'dividendRate', 'dividendYield', 'exDividendDate', 'payoutRatio', 'fiveYearAvgDividendYield', 'beta', 'trailingPE', 'forwardPE', 'volume', 'regularMarketVolume', 'averageVolume', 'averageVolume10days', 'averageDailyVolume10Day', 'marketCap', 'fiftyTwoWeekLow', 'fiftyTwoWeekHigh', 'priceToSalesTrailing12Months', 'fiftyDayAverage', 'twoHundredDayAverage', 'trailingAnnualDividendRate', 'trailingAnnualDividendYield', 'enterpriseValue', 'profitMargins', 'floatShares', 'sharesOutstanding', 'sharesShort', 'sharesShortPriorMonth', 'sharesShortPreviousMonthDate', 'dateShortInterest', 'sharesPercentSharesOut', 'heldPercentInsiders', 'heldPercentInstitutions', 'shortRatio', 'shortPercentOfFloat', 'impliedSharesOutstanding', 'bookValue', 'priceToBook', 'lastFiscalYearEnd', 'nextFiscalYearEnd', 'mostRecentQuarter', 'earningsQuarterlyGrowth', 'netIncomeToCommon', 'trailingEps', 'forwardEps', 'pegRatio', 'lastSplitFactor', 'lastSplitDate', 'enterpriseToRevenue', 'enterpriseToEbitda', '52WeekChange', 'SandP52WeekChange', 'lastDividendValue', 'lastDividendDate', 'firstTradeDateEpochUtc', 'currentPrice', 'targetHighPrice', 'targetLowPrice', 'targetMeanPrice', 'targetMedianPrice', 'recommendationMean', 'recommendationKey', 'numberOfAnalystOpinions', 'totalCash', 'totalCashPerShare', 'ebitda', 'totalDebt', 'quickRatio', 'currentRatio', 'totalRevenue', 'debtToEquity', 'revenuePerShare', 'returnOnAssets', 'returnOnEquity', 'freeCashflow', 'operatingCashflow', 'earningsGrowth', 'revenueGrowth', 'grossMargins', 'ebitdaMargins', 'operatingMargins', 'trailingPegRatio']
+"""
+
+# Pandas Dataframe, might need to change where this is located to open
+df = pd.read_csv('Code\\data\\ticker_info_random1000.csv')
+
+# All possible tickers
+valid_tickers = df['Ticker']
+
+# allows us to access by column, ticker
+df.set_index('Ticker', inplace=True)
+
+# If you want the time series data
+dates = ast.literal_eval(df.at[valid_tickers[3], 'dates'])
+
+# as datetime object, makes it easiler to plot
+date_as_datetimeObj = pd.to_datetime(dates)
+
+stock_values = ast.literal_eval(df.loc[valid_tickers[3], 'time_series'])
+
+plt.plot(date_as_datetimeObj, stock_values)
+plt.show()
+
+# To get ticker information
+compinsation_risk = df.at[valid_tickers[3], 'compensationRisk']
