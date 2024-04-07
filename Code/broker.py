43a43455542954110d158b1603693337592980e5
Rachael Judy <rjudy@cedarville.edu>
2024-04-06 21:02:01 -0400

    start integration

--- Code/broker.py
+++ Code/broker.py
@@ -1,3 +1,4 @@
+import ast
 import collections
 import math
 import random
@@ -7,11 +8,11 @@ import random
 # also need to define randomized order/buy amount of some kind
 # need to figure out if risk reward balance plays in
 class Broker:
-    def __init__(self, id: int, initial_money: float, risk_minimum: float=0, risk_maximum: float=math.inf,
-                 purchase_rate_limit: int=0, neighbor_weight: float=0, shift_influence = False):
+    def __init__(self, id: int, initial_money: float, risk_minimum: float = 0, risk_maximum: float = math.inf,
+                 purchase_rate_limit: int = 0, neighbor_weight: float = 0, shift_influence: bool = False):
         self.id = id
         self.money = initial_money
-        self._portfolio = dict()  # contains percent improvements in last period for owned stocks?
+        self._portfolio = collections.defaultdict(lambda: 0)  # stock to quantity dictionary
 
         self.neighbor_weight = neighbor_weight
 
@@ -20,102 +21,172 @@ class Broker:
         self.current_risk = 0
 
         self.adjust_influence = shift_influence
-        self.neighbor_history = dict()
+        self.neighbor_history = dict()  # how do we want to use this?
 
         # possible configuration values for later
         self._purchase_rate = purchase_rate_limit
 
     # buy and sell functionality based on allowed purchase rate, outside influence,
-    def update(self, graph, available_stocks, id_to_broker_map):
+    def update(self, graph, available_stocks, id_to_broker_map, stocks, date):
         # include transaction, update status
-        self.update_current_risk(available_stocks)
+        self.assess_portfolio_risk(stocks, date)
 
         # assess risk of buying/selling each stock
         risk_dict = dict()
         for stock in available_stocks:
-            risk_dict[stock] = self.get_risk(stock, available_stocks) + self.neighbor_weight * self.get_neighbor_factor(graph, available_stocks, id_to_broker_map)
-
+            risk_dict[stock] = (self.assess_risk(stock, stocks, date) + self.neighbor_weight *
+                                self.get_neighbor_factor(graph, available_stocks, id_to_broker_map, stocks, date)[stock])
+        print("finished individiual assessments")
         while self.current_risk < self.preferred_risk_minimum or self.current_risk > self.preferred_risk_maximum or random.random() < .05:
             # is there a better way to choose then randomly selecting higher or lower risk
-
-
-
+            print(self.current_risk)
             # if large gap, pull from back, if small from front half?
             if self.preferred_risk_minimum > self.current_risk:
                 risk_dict = dict()
                 for stock in available_stocks:
-                    risk_dict[stock] = self.get_risk(stock, available_stocks) + self.get_neighbor_factor(graph,
-                                                                                                         available_stocks,
-                                                                                                         id_to_broker_map)
-
-                sorted_list = sorted(risk_dict.keys(), key=lambda x: x[1])
+                    risk_dict[stock] = self.assess_risk(stock, stocks, date) \
+                                       + self.get_neighbor_factor(graph, available_stocks, id_to_broker_map, stocks, date)[stock]
+                sorted_list = sorted(risk_dict.keys(), key=risk_dict.get)
                 if self.preferred_risk_minimum - self.current_risk > .5 * self.preferred_risk_minimum:  # tiny bit arbitrary
                     # purchase random sample from upper half
-                    plan = random.sample(list(sorted_list[len(sorted_list)//2:]))
+                    plan = random.sample(list(sorted_list[len(sorted_list) // 2:]), 1)[0]
                 else:
-                    plan = random.sample(list(sorted_list[:len(sorted_list)//2]))
-                self.money -= available_stocks[plan].price
+                    plan = random.sample(list(sorted_list[:len(sorted_list) // 2]), 1)[0]
+                self.money -= stocks[plan].date_to_price[date]
                 self._portfolio[plan] += 1
 
             elif self.preferred_risk_maximum < self.current_risk:
                 risk_dict = dict()
                 for stock in self._portfolio:
-                    risk_dict[stock] = self.get_risk(stock, available_stocks) + self.get_neighbor_factor(graph,
-                                                                                                         available_stocks,
-                                                                                                         id_to_broker_map)
+                    risk_dict[stock] = (self.assess_risk(stock, stocks, date)
+                                        + self.get_neighbor_factor(graph, available_stocks, id_to_broker_map, stocks, date)[stock])
 
-                sorted_list = sorted(risk_dict.keys(), key=lambda x: x[1])
+                sorted_list = sorted(risk_dict.keys(), key=risk_dict.get)
                 if self.preferred_risk_minimum - self.current_risk > .5 * self.preferred_risk_minimum:  # tiny bit arbitrary
                     # purchase random sample from upper half
-                    plan = random.sample(list(sorted_list[len(sorted_list)//2:]))
+                    plan = random.sample(list(sorted_list[len(sorted_list) // 2:]), 1)[0]
                 else:
-                    plan = random.sample(list(sorted_list[:len(sorted_list)//2]))
-                self.money += available_stocks[plan].price
+                    plan = random.sample(list(sorted_list[:len(sorted_list) // 2]), 1)[0]
+                self.money += stocks[plan].date_to_price[date]
                 self._portfolio[plan] -= 1
 
-            self.update_current_risk(available_stocks)
+            self.assess_portfolio_risk(stocks, date)
 
         # update public status
         if self.adjust_influence:
             for neighbor in graph.in_edges(self.id):
-                graph.weights[neighbor][self.id] += .005 * ( # TODO: Parameterize this
-                            id_to_broker_map[neighbor].get_status(available_stocks) - self.neighbor_history[neighbor])
+                graph.weights[neighbor][self.id] += .005 * (  # TODO: Parameterize this
+                        id_to_broker_map[neighbor].get_status(available_stocks) - self.neighbor_history[neighbor])
                 self.neighbor_history[neighbor] = id_to_broker_map[neighbor].get_status(available_stocks)
 
-    def get_status(self, stock_map):
-        return sum(self._portfolio[ticker]*stock_map[ticker] for ticker in self._portfolio) + self.money
+    def get_status(self, stocks, date):
+        """
+        Calculates the value of the broker's stock portfolio given the status of the stocks on a given date assuming the
+            broker were to sell immediately
+        :param stocks: the stock pandas dataframe
+        :param date: date in format 'MM/DD/YYYY'
+        :return: float of current value of broker
+        """
 
-    def get_neighbor_factor(self, graph, available_stocks, id_to_broker_map):  # need to clean up parameter choices
+        portfolio_value = self.money
+
+        for ticker in self._portfolio:
+            portfolio_value += self._portfolio[ticker] * stocks[ticker].date_to_price[date]
+
+        return portfolio_value
+
+    def get_neighbor_factor(self, graph, available_stocks, id_to_broker_map, stocks, date):
         neighbors = graph.in_edges(self.id)
         stock_dict = collections.defaultdict(lambda: 0)
         # iterate through graph getting neighbors and values - simple weighting?
         for stock in available_stocks:
             for n in neighbors:
-                stock_dict[stock] += graph.weights[n][self.id] * id_to_broker_map[n].get_risk(stock, available_stocks)
-        return stock_dict
+                stock_dict[stock] += graph.weights[n][self.id] * id_to_broker_map[n].assess_risk(stock, stocks, date)
+        return stock_dict  # dictionary of neighbor risk assessment of each stock in the available stocks
 
-    def update_current_risk(self, available_stocks):
-        '''
-        Update the current risk based on the updated data
+    def get_stats_for_risk(self, ticker, stocks, date):
+        """
+        Computes the stats for Josh's risk functions
+        @param ticker: ticker for stock to assess
+        @param stocks: dataframe of stock data
+        @param date: date in format MM/DD/YYYY
+        :return: tuple of portfolio_allocation, forward_eps, earnings_growth, high, low, dividend_ratio, fifty_day_average
+        """
 
-        :param available_stocks:
+        # compute total equity for a given stock
+        index = stocks[ticker].dates.index(date)
 
-        :return: None
-        '''
-        # TODO: this will update based on new data
-        pass
+        current_amount = self._portfolio[ticker] * stocks[ticker].date_to_price[date]
+        portfolio_allocation = (current_amount + stocks[ticker].date_to_price[date]) / (
+                    self.get_status(stocks, date)
+                    - self.money + stocks[ticker].date_to_price[date])
 
-    def get_risk(self, ticker, available_stocks, include_portfolio=False) -> float:
-        '''
+        start = index - 52*7+1 if index >= 52*7-1 else 0
+        high = 0
+        low = math.inf
+        for d in range(start, index+1):
+            high = max(high, stocks[ticker].date_to_price[stocks[ticker].dates[d]])
+            low = min(low, stocks[ticker].date_to_price[stocks[ticker].dates[d]])
 
-        :param ticker: stock to assess
-        :param available_stocks: map of tickers to stock data, possible Stock class
-        :param include_portfolio: determines whether or not to include own portfolio when assessing risk
+        start = index - 49 if index >= 49 else 0
+        average = 0
+        for d in range(start, index+1):
+            average += stocks[ticker].date_to_price[stocks[ticker].dates[d]]
+        average /= 52
 
-        :return: normalized float with risk for a given stock
-        '''
+        return portfolio_allocation, high, low, average
 
-        # TODO: populate this with computation for risk given stats - need something that says risk of buying or selling
-        # @Josh is this calculation going to account for history? might need another parameter in Stock for that
-        pass
+    def assess_risk(self, ticker, stocks, date):
+        """
+        Assess the risk using a fat-tailed distribution formula.
+        @param ticker: ticker for stock to assess
+        @param stocks: dataframe of stock data
+        @param date: date in format MM/DD/YYYY
 
+        Returns:
+            float: Risk assessment for a single stock.
+        """
+        portfolio_allocation, high, low, fifty_day_average = self.get_stats_for_risk(ticker, stocks, date)
+
+        k = 3  # Constant for fat-tailed distribution
+
+        x = math.exp(abs(high - low - stocks[ticker].dividend_ratio * fifty_day_average) * -k)  # Equation to express volatility
+        a = portfolio_allocation * stocks[ticker].forward_eps * (
+                    1 - stocks[ticker].earnings_growth)  # Equation to express likelihood (a weighted sum of significant metrics)
+
+        return a * (x) if not math.isnan(a*x) else 0
+
+    def assess_portfolio_risk(self, stocks, date):
+        """
+        Assess the risk of the entire portfolio using a fat-tailed distribution formula.
+
+        @param stocks: dataframe of stock data
+        @param date: date in format MM/DD/YYYY
+
+        Returns:
+            float: Risk assessment for the entire portfolio.
+
+        """
+        portfolio_allocation, forward_eps_list, earnings_growth_list, high_list, low_list, dividend_ratio_list, fifty_day_average_list = list(), list(), list(), list(), list(), list(), list()
+
+        for ticker in self._portfolio:
+            pa, h, l, fda = self.get_stats_for_risk(ticker, stocks, date)
+            portfolio_allocation.append(pa)
+            forward_eps_list.append(stocks[ticker].forward_eps)
+            earnings_growth_list.append(stocks[ticker].earnings_growth)
+            high_list.append(h)
+            low_list.append(l)
+            dividend_ratio_list.append(stocks[ticker].dividend_ratio)
+            fifty_day_average_list.append(fda)
+
+        k = 3  # Constant for fat-tailed distribution
+        total_risk = 0
+        for i in range(len(portfolio_allocation)):
+            x = math.exp(abs(high_list[i] - low_list[i] - dividend_ratio_list[i] * fifty_day_average_list[i]) * -k)  # volatility
+            a = portfolio_allocation[i] * forward_eps_list[i] * (
+                    1 - earnings_growth_list[i])  # likelihood
+            individual_risk = a * x
+            total_risk += individual_risk
+
+        self.current_risk = total_risk
+        return total_risk
--- Code/simulation.py
+++ Code/simulation.py
@@ -1,33 +1,73 @@
+import ast
+from datetime import datetime, timedelta
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import pandas as pd
 import powerlaw
 import random
 
 from broker import Broker
+from stock import Stock
 
 # will have directed graph indicating how much influence
 broker_network = nx.DiGraph()
 
 # we need to pull in data over relevant intervals - maybe
-# create list of brokers
+stock_df = pd.read_csv('data/ticker_info_random1000.csv')
+valid_tickers = stock_df['Ticker']
+stock_df.set_index('Ticker', inplace=True)
 
+# convert to dictionary of ticker to Stock class
+stocks = {}
+for ticker in valid_tickers:
+    # compute total equity for a given stock
+    try:
+        dates = ast.literal_eval(stock_df.at[ticker, 'dates'])
+        prices = ast.literal_eval(stock_df.at[ticker, 'time_series'])
+    except ValueError:
+        # print(ticker)
+        pass
+
+    forward_eps = stock_df['forwardEps'][ticker]
+    earnings_growth = stock_df['earningsGrowth'][ticker]
+    dividend_ratio = stock_df['dividendRate'][ticker]
+
+    if isinstance(forward_eps, pd.Series):
+        forward_eps = forward_eps[0]
+        earnings_growth = earnings_growth[0]
+        dividend_ratio = dividend_ratio[0]
+
+    if math.isnan(dividend_ratio):
+        dividend_ratio = 0
+        earnings_growth = .05
+        forward_eps = 2
+
+    stocks[ticker] = Stock(ticker, forward_eps, earnings_growth, dividend_ratio, dates, prices)
+
+
+# create list of brokers
 N = 100
 standard_influence = .2
 time_steps = 1000
-epochs = 100
+trials = 100
+start = "01/01/2003"
+end = "12/31/2023"
+
+# test over 1980 to 2000 and then 2003 to 2023
 
 # figure out what data to collect and plot from the simulation
 
-for epoch in range(epochs):
+for trial in range(trials):
     # (self, id: int, initial_money: float, risk_minimum: float=0, risk_maximum: float=math.inf,
     #                  purchase_rate_limit: int=0, neighbor_weight: float=0, shift_influence = False)
     # brokers will need to set these values based on if we're doing a influence shift or a risk variety
 
-    brokers = [Broker(i,  100) for i in range(N)]
+    brokers = [Broker(i, 1_000_000, 50, 100, 0, 0.05, False)
+               for i in range(N)]
 
-    # Generate some synthetic data following a power law distribution
+    # Generate the number of friends and populate those friend relationships with a normal distribution
     friend_count_dist = np.floor(np.array(powerlaw.Power_Law(xmin=1, parameters=[3.0]).generate_random(N)) + 2)
 
     """
@@ -46,22 +86,39 @@ for epoch in range(epochs):
             neighbor = np.random.normal(N/2, .34*N)
             broker_network.add_edge(i, neighbor, weight=standard_influence/friend_count_dist[i])
 
+    # nx.draw(broker_network, with_labels=True, font_color='white', node_shape='s')
+    # plt.show()
 
-    nx.draw(broker_network, with_labels=True, font_color='white', node_shape='s')
-    plt.show()
-
-    for step in range(time_steps):
+    start_date = datetime.strptime(start, "%m/%d/%Y")
+    end_date = datetime.strptime(end, "%m/%d/%Y")
+    date = start_date
+    delta = timedelta(days=1)
+    while date < end_date:
         random.shuffle(brokers)
         # TODO: replace with data for the time step, possible turn into map of stock to price/quantity available
-        available_stocks = {}
+        available_stocks = set()
 
-        for broker in brokers:
-            broker.update(broker_network, available_stocks)
+        # iterate through all stocks in dataframe and add ones that exist at this time to the avialble set
+        for ticker in stocks:
+            stock = stocks[ticker]
+            try:
+                if date.strftime("%m/%d/%Y") in stock.dates:
+                    available_stocks.add(ticker)
+            except TypeError:
+                # print(ticker)
+                pass
+
+        if len(available_stocks):
+            for broker in brokers:
+                broker.update(broker_network, available_stocks, brokers, stocks, date.strftime("%m/%d/%Y"))
+
+        date += delta
+        print(date.strftime("%m/%d/%Y"))
 
     # examine state of brokers at end
+        for broker in brokers:
+            print(broker.id, broker.get_status(stocks, date))
 
-    for broker in brokers:
-        print(broker.get_status())
 
 # add plotting code from whatever we decide to plot
 
